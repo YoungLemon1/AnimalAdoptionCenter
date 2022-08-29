be34d818f6bb5ac82df88b72cc0941c6b1cc2fc2
YoungLemon1 <110844957+YoungLemon1@users.noreply.github.com>
2022-08-29 18:58:05 +0300

    Modified program.cs to work with the new database.

--- AnimalAdoptionCenter/Models/SubCategory.cs
+++ AnimalAdoptionCenter/Models/SubCategory.cs
@@ -7,4 +7,3 @@
         public Category? ParentCategory { get; set; }
     }
 }
-;
--- AnimalAdoptionCenter/Program.cs
+++ AnimalAdoptionCenter/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using AnimalAdoptionCenter.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AnimalAdoptionCenterContext>(options => options.UseSqlite("Data Source=C:\\temp\\AnimalAdoptionCenterDb.db"));
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var ctx = scope.ServiceProvider.GetRequiredService<AnimalAdoptionCenterContext>();
+    ctx.Database.EnsureDeleted();
+    ctx.Database.EnsureCreated();
+}
+
 app.UseRouting();
 app.UseStaticFiles();
 app.UseEndpoints(endpoints =>
