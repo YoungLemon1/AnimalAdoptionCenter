c2457dd5dd4ffe4e12492d058e0123578b5a77fd
YoungLemon1 <110844957+YoungLemon1@users.noreply.github.com>
2022-08-29 18:52:15 +0300

    Added ad DbContext class and added initial entities that will be created with the database

---
+++ AnimalAdoptionCenter/Data/AnimalAdoptionCenterContext.cs
@@ -0,0 +1,23 @@
+﻿using AnimalAdoptionCenter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalAdoptionCenter.Data
+{
+    public class AnimalAdoptionCenterContext : DbContext
+    {
+        public AnimalAdoptionCenterContext(DbContextOptions<AnimalAdoptionCenterContext> options) : base(options)
+        {
+        }
+        public DbSet<Animal>? Animals { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Customer>? Customers { get; set; }
+        public DbSet<City>? Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new { CategoryId = 1, CategoryName = "Dog" },
+                new { CategoryId = 2, CategoryName = "Cat" });
+        }
+    }
+}
