472ee95f2db4594267a28025f81b5a8df886500f
YoungLemon1 <110844957+YoungLemon1@users.noreply.github.com>
2022-09-04 00:12:59 +0300

    added password to customer

---
+++ AnimalAdoptionCenter/Controllers/AnimalController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AnimalAdoptionCenter.Models;
+using AnimalAdoptionCenter.Services;
+using AnimalAdoptionCenter.Services.Repositories;
+
+namespace AnimalAdoptionCenter.Controllers
+{
+    public class AnimalController : Controller
+    {
+        private IRepository _repository;
+
+        public AnimalController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddAnimal()
+        {
+
+        }
+    }
+}
--- AnimalAdoptionCenter/Models/Customer.cs
+++ AnimalAdoptionCenter/Models/Customer.cs
@@ -5,9 +5,9 @@
         public int CustomerId { get; set; }
         public string? CustomerName { get; set; }
         public string? UserName { get; set; }
+        public string? Password { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        //My changes
         public City? City { get; set; }
     }
 }
