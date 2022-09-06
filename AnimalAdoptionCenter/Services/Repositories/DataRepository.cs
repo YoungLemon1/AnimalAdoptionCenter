79d9f9cea79c2e068240da4cea5cafa06213eb44
YoungLemon1 <110844957+YoungLemon1@users.noreply.github.com>
2022-09-07 00:49:30 +0300

    Added an ajax prompt on creating animal

--- AnimalAdoptionCenter/Services/Repositories/DataRepository.cs
+++ AnimalAdoptionCenter/Services/Repositories/DataRepository.cs
@@ -18,14 +18,13 @@ namespace AnimalAdoptionCenter.Services.Repositories
         {
             return _context.Animals!;
         }
-        public Animal GetAnimalById(int id)
+        Animal IRepository.GetAnimalById(int id)
         {
-            return _context.Animals!.Single(animal => animal.Id == id);
+            return _context.Animals!.Single(a => a.Id == id);
         }
-
-        public Animal GetAnimalByName(string name)
+        Animal IRepository.GetAnimalByName(string name)
         {
-            return _context.Animals!.Single(animal => animal.Name == name);
+            return _context.Animals!.Single(a => a.Name == name);
         }
         IEnumerable<Animal> IRepository.GetPopularAnimals(int num)
         {
@@ -69,8 +68,15 @@ namespace AnimalAdoptionCenter.Services.Repositories
         }
         void IRepository.InsertAnimal(Animal animal)
         {
-            _context.Animals!.Add(animal);
-            _context.SaveChanges();
+            try
+            {
+                _context.Animals!.Add(animal);
+                _context.SaveChanges();
+            }
+            catch
+            {
+                return;
+            }
         }
         void IRepository.InsertCategory(Category category)
         {
@@ -85,7 +91,6 @@ namespace AnimalAdoptionCenter.Services.Repositories
         void IRepository.InsertComment(Comment comment, Animal animal)
         {
             _context.Comments!.Add(comment);
-            animal = GetAnimalById(comment.AnimalId);
             animal.Comments!.Add(comment);
             _context.SaveChanges();
         }
@@ -164,16 +169,6 @@ namespace AnimalAdoptionCenter.Services.Repositories
         {
             _context.SubCategories!.Remove(subCategory);
             _context.SaveChanges();
-        }
-
-        Animal IRepository.GetAnimalById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        Animal IRepository.GetAnimalByName(string name)
-        {
-            throw new NotImplementedException();
         }
     }
 }
---
+++ AnimalAdoptionCenter/wwwroot/javascript/Ajax-Form-Notify.js
@@ -0,0 +1,18 @@
+﻿function ajaxpost() {
+    window.alert("Animal created");
+    //// (A) GET FORM DATA
+    //var form = document.getElementById("newAnimalForm");
+    //var data = new FormData(form);
+
+    //// (B) AJAX REQUEST
+    //// (B1) POST DATA TO SERVER, RETURN RESPONSE AS TEXT
+    //fetch("C:\Users\yaniv\source\repos\Github\AnimalAdoptionCenter\master\V4\AnimalAdoptionCenter\Views\Animal\Index.cshtml", { method: "POST", body: data })
+    //    .then(res => res.text())
+
+    //    // (B2) SHOW MESSAGE ON SERVER RESPONSE
+    //    .then((response) => {
+    //        window.prompt(response);
+    //        if (response == "OK") { alert("SUCCESSFUL!"); }
+    //        else { alert("FAILURE!"); }
+    //    })
+}
